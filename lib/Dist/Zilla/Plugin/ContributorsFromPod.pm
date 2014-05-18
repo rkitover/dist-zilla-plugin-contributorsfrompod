8eca38b0c43bd4f6970b10cdadc94bad5f33c155
Rafael Kitover <rkitover@cpan.org>
2014-05-18 08:04:59 -0400

    initial commit

---
+++ lib/Dist/Zilla/Plugin/ContributorsFromPod.pm
@@ -0,0 +1,46 @@
+use utf8;
+package Dist::Zilla::Plugin::ContributorsFromPod;
+
+use strict;
+use warnings;
+use 5.008001;
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Plugin::ContributorsFromPod - abstract goes here
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::ContributorsFromPod;
+
+=head1 DESCRIPTION
+
+=cut
+
+# declarations go here
+
+=head1 FUNCTIONS/METHODS
+
+=head2 some_function
+
+=cut
+
+# some_function goes here
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<Some::Module>
+
+=back
+
+=head1 AUTHOR
+
+Rafael Kitover <rkitover@cpan.org>
+
+=cut
+
+__PACKAGE__; # End of Dist::Zilla::Plugin::ContributorsFromPod
